f5dc1fd7817abf1c33b3544c1dfc90a8ca069627
kathirvel13 <kathir13@proton.me>
2026-06-20 19:18:48 +0530

    Updated the current profile maker script

--- data/Obtain_Current_Profile.m
+++ data/Obtain_Current_Profile.m
@@ -1,11 +1,17 @@
+% This script can make a current profile based on the power consumed data
+
 data = readtable('House_18.csv');
 %head(data)
 %summary(data)
+
+% Finding total power from the given csv data
 P_total = sum(data{:,3:end},2);
 
-V_batt = 24;
+% Calculating current
+V_batt = 12;
 I_load = P_total ./ V_batt;
 
+% Making time-domain and introducing charging condition in given data
 t = (0:length(I_load)-1)';
 charge_idx = (t > 50000 & t < 120000) | (t > 270000 & t < 410000);
 I_load(charge_idx) = I_load(charge_idx) - 40;
