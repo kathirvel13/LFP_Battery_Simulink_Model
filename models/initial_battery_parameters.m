ea4b795e38eca3d6d2733974119a8892816d8e03
kathirvel13 <kathir13@proton.me>
2026-06-20 19:33:34 +0530

    Updated Battery capacity and initial parameters in simulink model

---
+++ models/initial_battery_parameters.m
@@ -0,0 +1,13 @@
+%% Battery Parameters
+
+Battery.Capacity_Ah = 30;
+
+Battery.Initial_SoC = 0.70;
+
+Battery.NominalVoltage = 12.8;
+
+Battery.MaxChargeCurrent = 30;
+
+Battery.MaxDischargeCurrent = 60;
+
+Battery.R0 = 0.03;
